46169d32619b8ca8d7945b0b83264f839f7e527e
phuc08898 <phuc08898@gmail.com>
2024-06-16 21:12:33 +0700

    update

--- index.js
+++ index.js
@@ -5,6 +5,13 @@ const port = 3000
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+console.log('Hello')
+console.log('Hello')
+console.log('Hello')
+console.log('Hello')
+console.log('Hello')
+console.log('Hello')
+console.log('Hello')
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
